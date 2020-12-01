761ba8754566844f4ad6dd178a44b7dbe7bd3a05
Pedro Alvarez <pedroangel.alvarez24@gmail.com>
2020-11-30 23:48:39 -0500

    Updates

---
+++ precache-manifest.c38f295f64f2ca17239f02ab3080280d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddf3c6c26c230353642f",
+    "url": "/palcoreact/static/css/main.25847f12.chunk.css"
+  },
+  {
+    "revision": "ddf3c6c26c230353642f",
+    "url": "/palcoreact/static/js/main.ddf3c6c2.chunk.js"
+  },
+  {
+    "revision": "7f61afdded323c872958",
+    "url": "/palcoreact/static/js/1.7f61afdd.chunk.js"
+  },
+  {
+    "revision": "58aaa14520154ed42dcd",
+    "url": "/palcoreact/static/js/runtime~main.58aaa145.js"
+  },
+  {
+    "revision": "3b7d9db59a2e5a9dfcaf87f753c0c739",
+    "url": "/palcoreact/index.html"
+  }
+];
